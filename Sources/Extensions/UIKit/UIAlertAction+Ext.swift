ba1c52bf7d65b43f9a915d111ef637f4e7f06769
Neil Smith <neilsmithdesign@gmail.com>
2019-08-28 16:48:21 +0200

    Add ok UIAlertAction

--- Sources/Extensions/UIKit/UIAlertAction+Ext.swift
+++ Sources/Extensions/UIKit/UIAlertAction+Ext.swift
@@ -9,10 +9,20 @@ import UIKit
 
 public extension UIAlertAction {
     
+    /// A standard cancel button which performs no action
     static var cancel: UIAlertAction {
         return .init(
             title: "Cancel",
             style: .cancel,
+            handler: nil
+        )
+    }
+    
+    /// A standard ok button which performs no action
+    static var ok: UIAlertAction {
+        return .init(
+            title: "Ok",
+            style: .default,
             handler: nil
         )
     }
