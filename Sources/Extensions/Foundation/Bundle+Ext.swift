9101a75a2e0d2bc6c7faf8eb9af3a9e84cfe257d
Neil Smith <neilsmithdesign@gmail.com>
2019-11-27 12:57:59 +0100

    Add convenience extension to Bundle for app version number

---
+++ Sources/Extensions/Foundation/Bundle+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  Bunde+Ext.swift
+//  
+//
+//  Created by Neil Smith on 27/11/2019.
+//
+
+import Foundation
+
+extension Bundle {
+    
+    var releaseVersionNumber: String? {
+        return infoDictionary?["CFBundleShortVersionString"] as? String
+    }
+    
+    var buildVersionNumber: String? {
+        return infoDictionary?["CFBundleVersion"] as? String
+    }
+    
+}
