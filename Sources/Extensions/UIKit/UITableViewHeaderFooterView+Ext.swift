0d471050cbac5df165b1891b24aaa4185a43a9cc
Neil Smith <neilsmithdesign@gmail.com>
2020-03-05 17:37:58 +0100

    Add reuseID convenience property to extension on UITableViewHeaderFooterView

---
+++ Sources/Extensions/UIKit/UITableViewHeaderFooterView+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  UITableViewHeaderFooterView.swift
+//  
+//
+//  Created by Neil Smith on 05/03/2020.
+//
+
+import UIKit
+
+public extension UITableViewHeaderFooterView {
+    
+    static var reuseID: String {
+        String(describing: self)
+    }
+    
+}
