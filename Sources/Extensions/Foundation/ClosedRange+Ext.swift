a9bdfeb426560a095753cf37e859f3be0c5fdd48
Neil Smith <neilsmithdesign@gmail.com>
2020-12-14 11:54:29 +0100

    Add convenience properties to ClosedRange.

--- Sources/Extensions/Foundation/ClosedRange+Ext.swift
+++ Sources/Extensions/Foundation/ClosedRange+Ext.swift
@@ -8,9 +8,19 @@
 import Foundation
 
 public extension ClosedRange where Bound == Double {
-    
     var midPoint: Double {
         return ((upperBound - lowerBound) / 2) + lowerBound
     }
-    
 }
+
+public extension ClosedRange {
+    var min: Bound { lowerBound }
+    var max: Bound { upperBound }
+}
+
+public extension ClosedRange where Bound: RawRepresentable, Bound.RawValue == Int {
+    var average: Double {
+        Double(min.rawValue + max.rawValue) / 2
+    }
+}
+
